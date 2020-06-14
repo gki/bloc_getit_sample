8edc3e1efd4618df4a443bca9fddf6703b7b747e
Gen Takeda <hogehoge.gki@gmail.com>
2020-06-14 15:58:06 +0900

    feat: add remove favorite function on favorite page

--- lib/widgets/bloc_favorite_page.dart
+++ lib/widgets/bloc_favorite_page.dart
@@ -3,36 +3,58 @@ import 'package:bloc_getit_sample/models/word_item.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 
-class BlocFavoritePage extends StatelessWidget {
+class BlocFavoritePage extends StatefulWidget {
   BlocFavoritePage();
 
   static const routeName = "/favorite";
 
   @override
+  _BlocFavoritePageState createState() => _BlocFavoritePageState();
+}
+
+class _BlocFavoritePageState extends State<BlocFavoritePage> {
+  final WordBloc wordBloc = GetIt.I<WordBloc>();
+  List<WordItem> wordItemList;
+
+  @override
+  void initState() {
+    super.initState();
+    wordBloc.items.listen((items) {
+      setState(() {
+        wordItemList = items;
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final word = GetIt.I<WordBloc>();
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Your Favorite"),
-      ),
-      body: StreamBuilder<List<WordItem>>(
-        stream: word.items,
-        builder: (context, snapshot) {
-          if (snapshot.data == null || snapshot.data.isEmpty) {
-            return Center(child: Text('Empty'));
-          }
-          final tiles = snapshot.data.map((item) {
-            return new ListTile(title: new Text(item.name));
-          });
+        appBar: AppBar(
+          title: Text("Your Favorite"),
+        ),
+        body: _buildBody());
+  }
 
-          final divided = ListTile.divideTiles(
-            context: context,
-            tiles: tiles,
-          ).toList();
+  Widget _buildBody() {
+    if (wordItemList.isEmpty) {
+      return Center(child: Text('Empty'));
+    }
 
-          return new ListView(children: divided);
-        },
-      ),
-    );
+    final tiles = wordItemList.map((item) {
+      return new ListTile(
+        title: new Text(item.name),
+        trailing: IconButton(
+          icon: Icon(Icons.delete),
+          onPressed: () => wordBloc.wordRemoval.add(WordRemoval(item.name)),
+        ),
+      );
+    });
+
+    final divided = ListTile.divideTiles(
+      context: context,
+      tiles: tiles,
+    ).toList();
+
+    return new ListView(children: divided);
   }
 }
